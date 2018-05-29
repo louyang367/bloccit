cefb5d27290b52e106b00258e99683eee5bde7c9
louyang <louyang367@gmail.com>
2018-05-29 14:29:45 -0700

    new route to /about

--- src/routes/static.js
+++ src/routes/static.js
@@ -4,5 +4,6 @@ const staticController = require("../controllers/staticController");
 const router = express.Router();
 
 router.get("/", staticController.index);
+router.get("/about", staticController.about);
 
 module.exports = router;
