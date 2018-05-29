394644f4fe3d5703254606ad120d0457737f1360
louyang <louyang367@gmail.com>
2018-05-29 12:33:30 -0700

    Separate controller & view folders; EJS view engine, landing page html & refactor into partials.

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const staticController = require("../controllers/staticController");
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
-});
+router.get("/", staticController.index);
 
 module.exports = router;
