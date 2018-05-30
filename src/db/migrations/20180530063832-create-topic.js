50e8bf478e2a408ec2ae879c78370dab49f45094
louyang <louyang367@gmail.com>
2018-05-30 10:13:07 -0700

    MVC code for topics, including db setup

---
+++ src/db/migrations/20180530063832-create-topic.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Topics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      topicId: {
+        type: DataTypes.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "topics",
+          key: "id",
+          as: "topicId",
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Topics');
+  }
+};
---
+++ src/routes/topics.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const topicController = require("../controllers/topicController")
+router.get("/topics", topicController.index);
+
+module.exports = router;
